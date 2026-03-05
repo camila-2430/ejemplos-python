136a2c7f6916c7f663db17a909a5189ae979f041
camila-2430 <camila.osorio.jf@gmail.com>
2026-03-05 01:08:58 +0000

    aprendiendo

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+print("hola mundo L")
+
+
+
+nombre = "camila"
+edad = 32
+altura = 1.53
+es_informatico = True
+ciudad = "talagante"
+
+
+
+#tipos de datos
+
+str #esto es un texto
+int #esto es un texto
+float #esto es un flotante 
+bool #esto es verdadero o falso
+
+
+#input
+#input()
+texto = input()
+print(texto)
