60ce78cecc33721f5a1970c8a72efdac676199ca
camila-2430 <camila.osorio.jf@gmail.com>
2026-03-13 22:01:40 +0000

    se edita documentacion

---
+++ diccionarios.py
@@ -0,0 +1,217 @@
+import os
+
+## diccionarios
+#una estructura de datos
+
+#que guarda datos en formato clave , valor
+
+#  clave  :    valor
+# "nombre":"cristian"
+
+
+persona = {
+    "nombre" : "carlos",
+    "edad" : 28,
+    "direccion": "calle falsa 123",
+    "ciudad" : "valparaiso"
+}
+
+
+# acceder a los valores de nuestro diccionario con ["keys"]
+#print(persona["ciudad"])
+
+#modificar valores
+
+persona["edad"] = 35
+persona["nombre"] = "carla"
+
+
+
+#agregar un dato nuevo 
+
+persona["pais"] = "chile"
+persona["altura"] = 190
+
+#print(persona)
+
+
+#  recorrer el diccionario
+# iterar keys
+""" for k in persona:
+    print(k)
+
+##iterar values
+for v in persona.values():
+    print(v)
+
+# iterar todo 
+for k , v in persona.items():
+    print(k,v) """
+
+
+""" #lista de diccionarios de alumnos 
+alumnos = []
+
+
+while True:
+
+    print("--------menu--------")
+    print("1. agregar alumno")
+    print("2. ver alumnos")
+    print("3. salir")
+
+    opcion = input("elige una opcion")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    if opcion == "1":
+        nombre = input("nombre de alumno")
+        edad = input("edad del alumno")
+
+        alumno = {
+            "nombre":nombre,
+            "edad": edad
+        }
+        alumnos.append(alumno)
+
+        print("alumno agragado!!!!")
+
+    elif opcion == "2":
+
+        print("------lista de alumnos-----")
+
+        for alumno in alumnos:
+            print(alumno["nombre"], "--",alumno["edad"])
+    
+    elif opcion == "3":
+        print("programa cerrado")
+        break
+
+    else:
+        print("opcion no correcta solo se acepta 1,2 y 3 programa cerrado")
+        break
+
+     """
+
+
+
+# proyecto de lista de peliculas netflix 
+
+""" pelicula = {
+    nombre : nombre
+    anio:anio
+    actores: [actor1,actor2...]
+    } """
+
+#peliculas=[{pelicula1},{pelicula2}...]
+
+
+#flujo del programa
+'''
+
+generar la lista vacia
+peliculas=[]
+
+
+MIENTRAS EL PROGREAMA ESTE CORRIENDO
+imprimir 
+menu 
+1. agregar pelicula a la lista
+2. borrar
+3. ver
+4. salir
+
+que el usuario entrege la opcion 
+
+si el usuario eligio 1
+        ingresamos nombre de la pelicula
+        ingresamos al anio
+        ingresar genero 
+        pelicula = {
+            nombre : nombre
+            anio:anio
+            genero:genero]
+            }
+        agregar la pelicula a la lista(append)
+        mesaje que la pelicula esta guardada
+
+si el usuario eligio 2 
+           ingresar la pelicula a borrar 
+           buscamos la pelicula a borrar
+           borramos
+   
+si el usuario eligio 3
+        ver listado
+si el usuario eligio
+        mensaje de salida 
+        break
+        
+si el usuario puso otra cosa el program se cierra
+
+'''
+
+peliculas=[]
+
+while True:
+    print("menu")
+    print("1. agregar pelicula a la lista")
+    print("2. borrar")
+    print("3. ver lista de pelis pelis")
+    print("salir")
+
+    opcion = input("ingrese la opcion")
+
+    if opcion == "1":
+        nombre = input("ingresar nombre de la pelicula")
+        genero = input("ingresar genero de la pelicula")
+        anio = input("ingresar anio de la pelicula")
+
+        pelicula = {
+            "nombre" : nombre,
+            "anio":anio,
+            "genero":genero
+            }
+        peliculas.append(pelicula)
+        print("pelicula esta guardada")    
+    elif opcion == "2":
+        peli_borrar = input("ingresa la peli a borrar")
+         
+        for peli in peliculas:
+            
+            if peli["nombre"] == peli_borrar:
+                peliculas.remove(peli)
+                print("peli borrada")
+                break
+    elif opcion == "3":
+        print(peliculas)
+
+        
+
+    elif opcion == "4":
+        print("programa terminado!!!")
+        break
+
+
+    else:
+        print("opcion no valida terminando programa")
+        break
+
+
+
+
+
+
+
+
+#quiero guardar datos de google maps en una base de datos
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,26 +1,186 @@
 
-
-print("hola mundo L")
-
-
-
-nombre = "camila"
-edad = 32
-altura = 1.53
-es_informatico = True
-ciudad = "talagante"
+import math
+import random
 
 
+#print("hola mundo cruel!!!!")
 
-#tipos de datos
+# VARIABLES
+nombre = "cristian"
+edad = 19
+altura = 2.00
+es_profesor = True
+ciudad = "valparaiso"
 
+
+#TIPOS DE DATOS
 str #esto es un texto
-int #esto es un texto
+int #esto es un entero
 float #esto es un flotante 
-bool #esto es verdadero o falso
+bool #esto es verdadero  o falso
+list
 
-
-#input
+#INPUT
 #input()
-texto = input()
-print(texto)
+
+
+#numero = int(input("escribe un numero : "))
+#resultado = numero + 1
+#print(resultado)
+#
+
+#INPUT
+""" precio = input("escribe un precio : ")# pedir por consola un dato tipo str
+precio = int(precio) #convertir a entero el str con int(precio)
+resultado = precio + 10 #el precio sumado + 10 ya que ahora es numero entero
+print(resultado)# imprimeme el precio en pantalla """
+
+## CONDICIONALES
+
+
+#if condicion:
+#    codigo
+#comparadores
+""" 
+== igual
+!= diferente
+> mayor
+< menor
+>= mayor o igual
+<= menor o igual 
+"""
+
+
+edad = 20
+
+""" if edad >= 18:
+    print("puede entrar a la disco")
+elif edad >= 50:
+    print("demaciado antiguo!!")
+else:
+    print("no puede entrar a la disco")     
+ """
+
+#BUCLES
+
+#WHILE
+#while condicion:
+#    codigo
+# while significa mientras pase algo
+contador = 0
+
+""" while contador < 10:
+    print(contador)
+    contador += 1
+ """
+""" while True:
+    texto = input("escribe para salir de bucle")
+
+    if texto == "salir":
+        break """
+
+
+#FOR
+
+#for elemento in lista
+#    codigo
+""" lista = ["hola","a","todos"]
+
+for i in lista:
+    print(i) """
+
+
+## tipos de datos 2 
+
+## listas
+lista = [1,2,3,4,5,6]  ## corchetes son importantes
+## 
+diccionario = {"key":"value"}
+##sets
+sets={1,2,3,4,5}
+##tuplas
+tuplas = (1,2,3,4,5,6)
+
+
+
+## listas
+##index
+frutas = ["manzana","uva","kiwi","sandia", [1,2,3,4,[5,6,7]]]
+    #index   0        1      2      3           4         = posicion 
+    #        -5      -4     -3     -2            -1
+frutas[3] = "pera"
+frutas[0] = "algo"
+frutas[4] = [] #lista vacia
+
+""" print(frutas[2])
+print(frutas[1])
+print(frutas[0])
+print(frutas[4][4][0]) """
+
+""" print(frutas[-4])
+
+#print(dir(diccionario))
+
+#append
+frutas.append("naranja")  #agrega un item al final de la lista 
+
+##remove
+frutas.remove("kiwi")
+
+print(frutas)
+
+personas = ["cristian","esteban"]
+
+for i in personas:
+    print(i)
+ """
+ 
+""" alumnos = [] #lista vacia
+
+while True:
+    print("----------menu")
+    print("opcion 1 agregar")
+    print("opcion 2 borrar")
+    print("opcion 3 actualizar")
+    print("opcion 4 salir")
+    opcion = input("ingresa la opcion :")
+
+
+    if opcion == "1":
+        nombre = input("ingresa el nombre alumno")
+        alumnos.append(nombre)
+        print(alumnos)
+    elif opcion == "2":
+        nombre = input("ingresa el nombre alumno")
+        alumnos.remove(nombre)
+        prin[t(alumnos)
+    elif opcion == 3:
+        print("buscar alumno")
+    else:
+        break  """
+
+alumnos = ["carlos", "manuel"]
+
+for i in range(0):
+    print(i)
+
+#rango si le paso 4 me genera = 0,1,2,3
+#10  = 0,1,2,3,4,5,6,7,8,9
+
+
+numeros = [1,2,3,4,5,6,7,8,9,10,1,2,4,5,6,7,8,9,8,7,6,5,4,3,2,1]
+ordenados = sorted(numeros , reverse=True)
+print(ordenados)
+
+
+#slices donde corto
+texto = "palabra"
+palindromo = "palabra"
+if palindromo == texto[::-1]:
+    print("esto es un palindromo!!!!")
+
+print(texto[::-1]) 
+print(texto.count("a"))
+    
+
+##diccionarios
